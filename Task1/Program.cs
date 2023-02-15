dc69b897c6a5ddaed06d7354dce82d130ca6088d
Valentin.Kostin <frezer.2021@mail.ru>
2023-02-15 16:11:58 +0300

    start

---
+++ Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задача 64: Задайте значение N. Напишите программу, которая 
+выведет все натуральные числа в промежутке от N до 1. 
+Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
+
+int PrintNumbers(int num)
+{
+    if (num == 1) return 0;
+    Console.Write(num + ", ");
+    PrintNumbers(num - 1);    
+    return 1;
+}
+
+int GetNum(string text)
+{
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int n = GetNum("Введите число N: ");
+
+Console.Write($"N = {n} -> ");
+Console.Write($"{PrintNumbers(n)}");
---
+++ Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу,
+которая найдёт сумму натуральных элементов в промежутке
+от M до N. Выполнить с помощью рекурсии.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+
+int SumNum(int numM, int numN)
+{
+    if (numM == numN + 1) return 0;
+    return numM + SumNum(numM + 1, numN);
+}
+
+int GetNum(string text)
+{
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int m = GetNum("Введите число M: ");
+int n = GetNum("Введите число N: ");
+
+Console.Write($"M = {m}; N = {n} -> {SumNum(m, n)}");
