146d71ff391ee640c9c236cb4569f2555419eb9c
Valentin <frezer.2021@mail.ru>
2023-02-16 17:16:31 +0300

    gotovo

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -3,3 +3,24 @@
 Даны два неотрицательных числа m и n.
 m = 2, n = 3 -> A(m,n) = 9
 m = 3, n = 2 -> A(m,n) = 29*/
+
+int GetNumFromUser(string text)
+{
+    Console.Write(text);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+int numbM = GetNumFromUser("Введите число M:");
+int numbN = GetNumFromUser("Введите число N:");
+
+///Метод вычисления функции Аккермана:
+int AckermannFunction(int M, int N)
+{
+    if (M == 0) return N + 1;
+    if (M != 0 && N == 0) return AckermannFunction(M - 1, 1);
+    if (M > 0 && N > 0) return AckermannFunction(M - 1, AckermannFunction(M, N - 1));
+    return AckermannFunction(M, N);
+}
+
+Console.WriteLine($"Функция Аккермана для чисел A({numbM},{numbN}) = {AckermannFunction(numbM, numbN)}");
